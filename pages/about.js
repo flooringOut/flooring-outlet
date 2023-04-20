d60311a9b77ae06f6b4f26125c08a998d7c1695e
qaisw96 <qaisw14@gmail.com>
2023-04-20 14:49:51 +0300

    about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,66 @@
+import Image from 'next/image';
 import React from 'react';
+import { GiRotaryPhone } from 'react-icons/gi';
+import { FaFax } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
 
 function about() {
-  return <div>about</div>;
+  const contactItems = [
+    {
+      id: 1,
+      title: 'PHONE',
+      icon: <GiRotaryPhone size={52} />,
+      value: '6788953080',
+      // href: `tel:${tel}`,
+    },
+    {
+      id: 2,
+      title: 'ADDRESS',
+      icon: <FaFax size={38} />,
+      value: '3020 commerce way se , Atlanta, GA, U.S 30354',
+      // href: `tel:${tel}`,
+    },
+    {
+      id: 3,
+      title: 'EMAIL',
+      icon: <MdEmail size={42} />,
+      value: 'example55@gmail.com',
+      // href: `mailto:${email}`,
+    },
+  ];
+
+  return (
+    <div className='mt-20'>
+      <div className='container flex gap-8 items-center flex-col md:flex-row'>
+        <Image
+          alt='about image'
+          src='/about/ab1.jpg'
+          width={500}
+          height={500}
+        />
+        <h3 className='text-base'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+        </h3>
+      </div>
+      <div className='bg-primary mt-44 py-20'>
+        <div className='container text-white flex flex-col md:flex-row gap-20 justify-center items-center'>
+          {contactItems.map((item) => (
+            <div key={item.id} className='flex flex-col gap-4 items-center'>
+              {item.icon}
+              <h2 className='text-md'>{item.title}</h2>
+              <a href={item.href}>
+                <h3 className='text-base opacity-70'>{item.value}</h3>
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import Image from 'next/image';
 import { Open_Sans } from 'next/font/google';
-import Header from '@/src/components/Header';
 import Carousel from '@/src/components/Carousel';
 import Products from '@/src/components/Products';
-import Footer from '@/src/components/Footer';
 
 const OpenSans = Open_Sans({ subsets: ['latin'] });
 
